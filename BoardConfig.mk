a31db87460c1c234193ca64f9bc388cb6f952c41
Brint E. Kriebel <github@bekit.net>
2011-01-17 20:55:53 -0800

    Add GPS shim hack

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,6 +44,9 @@ BOARD_PREBUILT_LIBAUDIO := true
 # Use dirty hack to allow froyo libaudio
 BOARD_USE_KINETO_COMPATIBILITY := true
 
+# Use GPS shim hack
+BOARD_USES_GPSSHIM := true
+
 BOARD_EGL_CFG := device/nvidia/harmony/egl.cfg
 
 # fix this up by examining /proc/mtd on a running device
