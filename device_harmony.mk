0da886759288421d1ba1a72551817930128e31d8
Brint E. Kriebel <github@bekit.net>
2011-01-18 08:20:04 -0800

    Revert "Add GPS shim hack"
    This reverts commit c1ad424ab3225aef5baa7569ef2ad137ee5969cd.

--- device_harmony.mk
+++ device_harmony.mk
@@ -20,8 +20,7 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
-    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
 
 $(call inherit-product-if-exists, vendor/nvidia/harmony/harmony-vendor.mk)
 
